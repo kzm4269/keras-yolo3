5aaa6bf5069f79a50c4ba937c6a0e47eeb1a378f
Kazuma Takahara <4269kzm@gmail.com>
2019-11-09 14:19:04 +0900

    representative_dataset_gen()を実装

--- convert_keras_tflite.py
+++ convert_keras_tflite.py
@@ -2,7 +2,6 @@ import tensorflow as tf
 import os
 import sys
 import numpy as np
-#from FileRW import CFileRW
 
 if sys.version_info.major >= 3:
     import pathlib
@@ -12,6 +11,7 @@ else:
 args = sys.argv
 print(args)
 os.environ["CUDA_VISIBLE_DEVICES"] = args[1]  # '-1':CPU '0':GPU-0
+dataset_root = argv[2]  # Cityscapesなどの画像データセット
 
 tf.compat.v1.enable_eager_execution()
 tf.compat.v1.logging.set_verbosity(tf.compat.v1.logging.DEBUG)
@@ -22,41 +22,26 @@ tflite_model = converter.convert()
 tf.compat.v1.logging.set_verbosity(tf.compat.v1.logging.INFO)
 converter.optimizations = [tf.lite.Optimize.DEFAULT]
 
+
 # 代表的なデータセット読み込み ===================================================
 # パラメータ
 
-
 def representative_dataset_gen():
-    WIDTH = 512
-    HEIGHT = 512
-    DATANUM = 200
-    SAMPLE = 10
-
-    # read data
-    FileRW = CFileRW()
-    # input data
-    input = FileRW.read(filename="../../../data/mult_new/10/Input_x512y512z1_hp16.raw", datatype="float32")
-    print("input:", input.shape, input.dtype)
-
-    # reshape
-    # input data
-    #input = input.reshape(DATANUM, HEIGHT, WIDTH, 1)
-    input = input.reshape(1, HEIGHT, WIDTH, 1)
-    print("input:", input.shape, input.dtype)
-
-    input_data = np.zeros(shape=(1,HEIGHT,WIDTH, 1), dtype="float32")
-    input_data = np.array(input, dtype="float32")
-
-    yield [input_data]
-
-    # for i in range(int(DATANUM/SAMPLE)):
-    #     choice = np.arange(DATANUM)
-    #     print(choice)
-    #     choice = np.random.choice(a=choice, size=choice.shape[0], replace=False)
-    #     print(choice)
-    #     input = input[choice]
-    #     print(i)
-    #     yield [input]
+    from tensorflow.keras.preprocessing.image import load_img
+    from pathlib import Path
+    from tqdm import tqdm
+    paths = list(Path(dataset_root).glob('**/*.jpg'))
+    for i, path in enumerate(tqdm(paths)):
+        image = np.asarray(load_img(str(path), target_size=(416, 416)))[..., :3]
+        if image.dtype == np.uint8:
+            image = image.astype(np.float32)
+            image /= 255
+        elif image.dtype == np.float64:
+            image = image.astype(np.float32)
+        else:
+            assert image.dtype == np.float32, (path, image.dtype)
+        yield [image[np.newaxis]]
+    
 
 converter.representative_dataset = representative_dataset_gen
 # ================================================================================
